c3705148ddaa218ea13727d77b0cb76608e8eb9c
dumrenarayan6 <dumrenarayan20@gmail.com>
2026-06-20 14:31:22 +0545

    add new line relationalfile

---
+++ learningcode/Arith.py
@@ -0,0 +1,14 @@
+A = int(input("Enter first number :"))
+B = int(input("Enter second number :"))
+sum =A+B
+print("Sum of two number", sum)
+sub = B-A
+print ("Subtraction of B-A", sub)
+mul = A*B
+print ("Multiplication of two number", mul)
+Div = A/B
+print ("Division of two number",Div)
+moddd = A%B
+print ("Modulus of two number", moddd)
+square = A**4
+print ("Square of A to the power B", square)
---
+++ learningcode/Relational.py
@@ -0,0 +1,19 @@
+# num1 = int(input("enter first number :"))
+# num2 = int(input("enter second number :"))
+# if(num1 == num2):
+#     print("Both number are equal")
+# elif(num1!= num2):
+#     print("both number are not equal")
+# elif(num1 >= num2):
+#     print("num1 is greater then num2")
+# elif(num1<=num2):
+#     print("num2 is greater the num1")
+# else:
+#     print("invalid number")            
+# logicaloperators
+print(not False)
+print(not True)
+val1 = True
+val2 = False
+print (val1 and val2)
+print (val1 or val2)
