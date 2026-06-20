273990fe8fa2897b4fb150f74096adcbdcc53393
dumrenarayan6 <dumrenarayan20@gmail.com>
2026-06-20 11:07:05 +0545

    Condition

---
+++ learningcode/Condition.py
@@ -0,0 +1,13 @@
+Colour = input("Enter the name of current traffic light colour :")
+if Colour == "Red":
+    print("Stop")
+elif Colour == "Yellow":
+    print("Ready for go")
+elif Colour == "Green":
+    print("Go")
+else:
+    print("Invalide colour")
+A = int(input("Enter your age :"))
+C ="vote" if A>=18 else "Not vote"
+print (C)
+
