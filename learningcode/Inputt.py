588846398ba5d97452223e8322b18a58f46d3dc0
dumrenarayan6 <dumrenarayan20@gmail.com>
2026-06-20 10:35:23 +0545

    New file

---
+++ learningcode/Inputt.py
@@ -0,0 +1,3 @@
+Name = input("Enter your full name ::")
+A = int(input("Enter your age ::"))
+print("my name is ::", Name , "and I am ::", A ,"years old")
---
+++ learningcode/division.py
@@ -0,0 +1,7 @@
+# divisiion
+A,B = 5,10
+print(A/B)
+print(A//B)
+C,D = -12,5
+print(C/D)
+print(C//D)
